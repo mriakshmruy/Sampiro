44bd7bb22214c0d8b0262b60172aa9464fea7998
Kash <uy.mariakashmir@gmail.com>
2025-08-27 22:26:03 -0400

    Modify prayers view

--- lib/features/prayers/presentation/widgets/prayers_view.dart
+++ lib/features/prayers/presentation/widgets/prayers_view.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:sampiro/core/resources/assets.gen.dart';
 import 'package:sampiro/core/routes/app_router.dart';
 import 'package:sampiro/l10n/l10n.dart';
 
@@ -11,19 +12,18 @@ class PrayersView extends StatelessWidget {
     final l10n = context.l10n;
     final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          l10n.prayers,
-          style: theme.textTheme.titleLarge?.copyWith(color: theme.colorScheme.surface),
-        ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.replaceRoute(const HomeRoute());
-          },
-          child: Text(l10n.goToHome),
-        ),
+      backgroundColor: theme.colorScheme.primary,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              background: Assets.images.headerPrayers.image(),
+              title: const Text('Prayers'),
+            ),
+          ),
+        ],
       ),
     );
   }
