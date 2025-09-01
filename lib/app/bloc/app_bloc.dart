f46172b1b59d43234b18e97cd45500fd3a14425f
Kash <uy.mariakashmir@gmail.com>
2025-09-01 05:50:35 -0400

    fix error

--- lib/app/bloc/app_bloc.dart
+++ lib/app/bloc/app_bloc.dart
@@ -1,9 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'app_bloc.freezed.dart';
 part 'app_event.dart';
 part 'app_state.dart';
-part 'app_bloc.freezed.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc() : super(const AppState()) {
--- lib/features/services/presentation/bloc/services_bloc.dart
+++ lib/features/services/presentation/bloc/services_bloc.dart
@@ -1,9 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'services_bloc.freezed.dart';
 part 'services_event.dart';
 part 'services_state.dart';
-part 'services_bloc.freezed.dart';
 
 class ServicesBloc extends Bloc<ServicesEvent, ServicesState> {
   ServicesBloc() : super(const ServicesState()) {
@@ -13,7 +13,5 @@ class ServicesBloc extends Bloc<ServicesEvent, ServicesState> {
   void _onServicesStarted(
     ServicesStarted event,
     Emitter<ServicesState> emit,
-  ) {
-    // TODO: implement event handler
-  }
+  ) {}
 }
